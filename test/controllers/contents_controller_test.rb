af8f455d4d6bd3d950b6e4fb8da60a4298e6dc4a
Masato Tokuse <masadinti@gmail.com>
2019-04-07 13:57:50 +0900

    add Contents Test

--- test/controllers/contents_controller_test.rb
+++ test/controllers/contents_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class ContentsControllerTest < ActionDispatch::IntegrationTest
+
   test "should get list" do
     get contents_list_url
     assert_response :success
+    assert_select "title", "List | Media App"
   end
 
 end
