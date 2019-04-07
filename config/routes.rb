abb8f878f37ad3a700fcf5ca8c158d5626a3580b
Masato Tokuse <masadinti@gmail.com>
2019-04-07 20:11:22 +0900

    Users#Create ,Error Messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'contents#list'
   get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create'
+  resources :users
 end
