f628a8f960ea07fe2d402f2228388d3668d02b2d
Masato Tokuse <masadinti@gmail.com>
2019-04-14 13:50:37 +0900

    ログイン機能の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています'
+      render 'new'
+    end
   end
 
   def destroy
