a042199b26de4c793c8fa3bb15a112365cca9468
Masato Tokuse <masadinti@gmail.com>
2019-05-06 14:50:14 +0900

    記事編集の結合テスト追加

--- test/integration/contents_update_test.rb
+++ test/integration/contents_update_test.rb
@@ -25,8 +25,37 @@ class ContentsUpdateTest < ActionDispatch::IntegrationTest
     assert_not_equal(@content_before.text, @content_after.text)
   end
 
-  # invalid
-  # not login
-  # another user login
+  test "invalid content information" do
+    # ログインする
+    logged_in @user
+
+    @content_before = @user.contents.first
+    get content_path(@content_before)
+    assert_select "a[href=?]", edit_content_path(@content_before)
+
+    get edit_content_path(@content_before)
+    assert_template 'contents/edit'
+
+    patch post_path(@content_before), params: { content: { id: @content_before.id, title: "", text: "" } }
+    assert_template 'contents/edit'
+    assert_select "div#error_explanation"
+    assert_select "div.alert-danger"
+  end
+
+  test "not login" do
+    @content_before = @user.contents.first
+    get edit_content_path(@content_before)
+    not_logged_in_user
+  end
+
+  test "user cannot edit another user's content" do
+    # 別ユーザーでログインする
+    @another_user = users(:another)
+    logged_in @another_user
+
+    @content_before = @user.contents.first
+    get content_path(@content_before)
+    assert_select "a[href=?]", edit_content_path(@content_before), count:0
+  end
 
 end
