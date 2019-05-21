7a3fed3d52803b5eafa37ab663b3cc791fd73ea2
Masato Tokuse <masadinti@gmail.com>
2019-05-22 01:38:52 +0900

    カードの背景色と文字色をランダム化

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,6 +3,7 @@ class Content < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   mount_uploader :image, ImageUploader
   validates :user_id, presence: true
-  validates :title, presence: true, length: {maximum: 50}
-  validates :text, presence: true, length: {maximum: 400}
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :text, presence: true, length: { maximum: 400 }
+  TEXT_BG_COLOR = ["text-white bg-primary", "text-white bg-secondary", "text-white bg-success", "text-white bg-danger", "text-white bg-warning", "text-white bg-info", "bg-light", "text-white bg-dark"]
 end
